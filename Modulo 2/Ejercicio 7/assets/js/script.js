f00d533d24859875b30e47641eaafffb101c8b8d
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2025-11-19 20:27:47 -0300

    Ejercicio 7

--- Modulo 2/Ejercicio 7/assets/js/script.js
+++ Modulo 2/Ejercicio 7/assets/js/script.js
@@ -3,7 +3,9 @@ function mostrarMensaje() {
     console.log("Mensaje mostrado en la consola");
 }
 
-function submitMessage(event) {
+const button  = document.getElementById('botonSubmit')
+
+button.addEventListener("click", function(event) {
     event.preventDefault();
     
     // Obtener valores según los IDs del formulario en contacto.html
@@ -15,27 +17,62 @@ function submitMessage(event) {
     const comentarios = document.getElementById('validationCustom05').value.trim();
     const acuerdo = document.getElementById('invalidCheck').checked;
 
+    const myDiv = document.getElementById('msj_val_1');
+    const myDiv2 = document.getElementById('msj_val_2');
+    const myDiv3 = document.getElementById('msj_val_3');
+    const myDiv4 = document.getElementById('msj_val_4');
+    const myDiv5 = document.getElementById('msj_val_5');
+    const myDiv6 = document.getElementById('msj_val_6');
+    const myDiv7 = document.getElementById('msj_val_7');
+    const myDiv8 = document.getElementById('msj_val_8');
+    myDiv8.innerHTML = ""
+
     // Validar campos
-    if (!nombre || !apellido || !email || !ciudad || !region || !comentarios) {
-        alert('Por favor complete todos los campos.');
-        return;
+    if (!nombre) {
+        myDiv.innerHTML = "<p class='text-danger'> El campo esta vacio, ingresa el  nombre</p>"
+    }else{
+        myDiv.innerHTML = ""
     }
-
+    if( !apellido ){
+        myDiv2.innerHTML = "<p class='text-danger' > El campo esta vacio, ingresa el  apellido</p>"
+    }else{
+        myDiv2.innerHTML = ""
+    }
+    if( !email ){
+        myDiv3.innerHTML = "<p class='text-danger' > El campo esta vacio, ingresa el email </p>"
+    }else{
+        myDiv3.innerHTML = ""
+    }
+    if( !ciudad ){
+        myDiv4.innerHTML = "<p class='text-danger' > El campo esta vacio, ingresa el ciudad </p>"
+    }else{
+        myDiv4.innerHTML = ""
+    }
+    if( !region ){
+        myDiv5.innerHTML = "<p class='text-danger' > El campo esta vacio, ingresa el region </p>"
+    }else{
+        myDiv5.innerHTML = ""
+    }
+    if( !comentarios ){
+        myDiv6.innerHTML = "<p class='text-danger' > El campo esta vacio, ingresa el comentarios </p>"
+    }else{
+        myDiv6.innerHTML = ""
+    }
     if (!acuerdo) {
-        alert('Debe aceptar el acuerdo de uso.');
-        return;
+        myDiv7.innerHTML = "<p class='text-danger' > Debe aceptar el acuerdo de uso. </p>"
+    }else{
+        myDiv7.innerHTML = ""
     }
 
-    // Datos a enviar
-    const data = { nombre, apellido, email, ciudad, region, comentarios };
-    console.log('Datos a enviar:', data);
+    if( nombre != ""  && apellido != "" && email != "" && ciudad != "" && region != "" && comentarios != "" && acuerdo ){
+        myDiv8.innerHTML = "<p class='text-success' > Se ha enviado correctamente el cormulario</p>"
+        alert('Mensaje enviado. ¡Gracias ' + nombre + '!');
+        // Datos a enviar
+        const data = { nombre, apellido, email, ciudad, region, comentarios };
+        console.log('Datos a enviar:', data);
+    }
 
-    // Mostrar confirmación
-    alert('Mensaje enviado. ¡Gracias ' + nombre + '!');
-    
-    // Redirigir (opcional, descomenta si existe el archivo)
-    // window.location.replace("./form_enviado.html");
-}
+})
 
 function toggleNav() {
     // alert("Función toggleNav ejecutada");
