6a5ac12bfc45cd5eb713ee56de2f99c7e10754ce
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2025-12-31 03:22:22 -0300

    feat: Arreglo para ver mensajes en HTML

--- Modulo 4/Ejercicio 4 - Entrega 4/assets/js/app.js
+++ Modulo 4/Ejercicio 4 - Entrega 4/assets/js/app.js
@@ -3,6 +3,10 @@
   Estas funciones simulan llamadas a una red. No las modifiques.
 */
 
+const callbacks = document.getElementById("callbacks")
+const promises = document.getElementById("promises")
+const asyncAwait = document.getElementById("async-await")
+
 // API para obtener datos de un usuario
 const obtenerUsuario = (id, callback) => {
   // Simula una demora aleatoria entre 0.5 y 1.5 segundos
@@ -14,6 +18,7 @@ const obtenerUsuario = (id, callback) => {
       return;
     }
     console.log(`Buscando usuario con ID: ${id}...`);
+    callbacks.innerHTML += `Buscando usuario con ID: ${id}...\n`
     const usuario = {
       id: id,
       nombre: "John Doe",
@@ -35,6 +40,7 @@ const obtenerPosts = (userId, callback) => {
       return;
     }
     console.log(`Buscando posts del usuario con ID: ${userId}...`);
+    callbacks.innerHTML += `Buscando posts del usuario con ID: ${userId}...\n`
     const posts = [
       { id: 101, titulo: "Mi primer post", contenido: "..." },
       { id: 102, titulo: "Mi segundo post", contenido: "..." },
@@ -55,6 +61,7 @@ const obtenerComentarios = (postId, callback) => {
       return;
     }
     console.log(`Buscando comentarios del post con ID: ${postId}...`);
+    callbacks.innerHTML += `Buscando comentarios del post con ID: ${postId}...\n`
     const comentarios = [
       { id: 1, texto: "¡Excelente post!" },
       { id: 2, texto: "Muy informativo, gracias." },
@@ -80,21 +87,30 @@ function pruebasCallbacks(){
   obtenerUsuario( 1 , (errorUser, usuario) => {
     if (errorUser) {
         console.error(errorUser);
+        callbacks.innerHTML += errorUser
         return;
     }
     obtenerPosts(usuario.id, (errorPosts, posts) => {
       if (errorPosts) {
         console.error(errorPosts);
+        callbacks.innerHTML += errorPosts
         return;
       }
       const primerPost = posts[0];
       obtenerComentarios(primerPost.id, (errorComentarios, comentarios) => {
       if (errorComentarios) {
         console.error(errorComentarios);
+        callbacks.innerHTML += errorComentarios
         return;
       }
+        let comentariosDetalle = ""
+        comentarios.forEach( comentario => {
+          comentariosDetalle += `Comentario id: ${comentario.id}, texto: ${comentario.texto}\n`
+        })
         console.log("Comentarios obtenidos del post:", comentarios);
         console.log("---------------------------------")
+        callbacks.innerHTML += `Comentarios obtenidos del post: \n${comentariosDetalle}
+        ---------------------------------`
       });
     });
   });
@@ -124,32 +140,35 @@ const newObtenerUsuario = (id) => {
         };
         resolve(usuario)
       }
-    }, demora);
+    }, 1000);
   })
   
 };
 
 // API para obtener los posts de un usuario
-const newObtenerPosts = (userId) => {
+const newObtenerPosts = (userId, isPromise) => {
   const demora = Math.random() * 1000 + 500;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (!userId) {
         reject("Error: ID de usuario no proporcionado para buscar posts.");
       } else {
-        console.log(`Buscando posts del usuario con ID: ${userId}...`);
+        if (isPromise){
+          console.log(`Buscando posts del usuario con ID: ${userId}...`);
+          promises.innerHTML += `Buscando posts del usuario con ID: ${userId}...\n`
+        }
         const posts = [
           { id: 101, titulo: "Mi primer post", contenido: "..." },
           { id: 102, titulo: "Mi segundo post", contenido: "..." },
         ];
         resolve(posts);
       }
-    }, demora )}
-  )
+    }, 2000);
+  });
 };
 
 // API para obtener los comentarios de un post
-const newObtenerComentarios = (postId) => {
+const newObtenerComentarios = (postId, isPromise) => {
   const demora = Math.random() * 1000 + 500;
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -157,74 +176,90 @@ const newObtenerComentarios = (postId) => {
         reject(
           "Error: ID de post no proporcionado para buscar comentarios.");
       } else{
-        console.log(`Buscando comentarios del post con ID: ${postId}...`);
+        if (isPromise){
+          console.log(`Buscando comentarios del post con ID: ${postId}...`);
+          promises.innerHTML += `Buscando comentarios del post con ID: ${postId}...\n`
+        }
         const comentarios = [
           { id: 1, texto: "¡Excelente post!" },
           { id: 2, texto: "Muy informativo, gracias." },
         ];
         resolve(comentarios);
       }
-    }, demora);
+    }, 3000);
   })
   
 };
 
 function pruebasPromesas(){
   newObtenerUsuario(1)
-  .then(usuario =>{ 
-    newObtenerPosts(usuario.id)
-    .then(posts => {newObtenerComentarios(posts[0].id)
-      .then(comentarios => {
+  .then(usuario => newObtenerPosts(usuario.id, true))
+  .then(posts => newObtenerComentarios(posts[0].id, true))
+  .then(comentarios => {
         comentarios.forEach(comentario => {
           console.log(`Comentario id:${comentario.id} contiene el texto: ${comentario.texto} `)
+          promises.innerHTML += `Comentario id:${comentario.id} contiene el texto: ${comentario.texto} \n`
         });
-      })
-      .catch(error => {console.log(`Error encontrado: \n ${error}`)})
-    })
-  })
-}
+        promises.innerHTML += "      ---------------------------------"
+  }).catch(error => {console.log(`Error encontrado: \n ${error}`) 
+    promises.innerHTML += `Error encontrado: \n ${error}\n`})
+  }
 
 async function pruebaAsync() {
     try {
         console.log("Iniciando obtención de datos...");
+        asyncAwait.innerHTML += "Iniciando obtención de datos...\n"
         const user = await newObtenerUsuario(1);
         console.log(`El nombre de la organización es: ${user.id}`);
+        asyncAwait.innerHTML += `El nombre de la organización es: ${user.id}\n`
 
-        const posts = await newObtenerPosts(user.id);
+        const posts = await newObtenerPosts(user.id, false);
 
-        const comentarios = await newObtenerComentarios(posts[0].id);
+        const comentarios = await newObtenerComentarios(posts[0].id, false);
 
         comentarios.forEach( comentario => {
           console.log("ID comentario: "+comentario.id + "\n" + comentario.texto)
+          asyncAwait.innerHTML += "ID comentario: "+comentario.id + "\n" + comentario.texto + "\n"
         })
 
     } catch (error) {
         console.error("Error codigo " + error.status + ". \nFalló la operación asíncrona: "+ error.message);
     } finally {
         console.log("Proceso async/await finalizado.");
+        asyncAwait.innerHTML += "Proceso async/await finalizado.\n"
+        asyncAwait.innerHTML +=   "        ---------------------------------"
     }
 }
 
 const pruebas = () => {
   setTimeout(() => {
-      console.log("---------------------------------")
-      console.log("Prueba de funciones con Callbacks")
-      console.log("---------------------------------")
+      callbacks.innerHTML += `
+      ---------------------------------
+      Prueba de funciones con Callbacks
+      ---------------------------------\n`
       pruebasCallbacks()
-    }, 3000)
+    }, 1000)
   setTimeout(() => {
+      promises.innerHTML += `
+      ---------------------------------
+      Prueba de funciones con Promises
+      ---------------------------------\n`
       console.log("---------------------------------")
       console.log("Prueba de funciones con Promises ")
       console.log("---------------------------------")
       pruebasPromesas()
-    }, 6000)
+    }, 8000)
   setTimeout(() => {
+      asyncAwait.innerHTML += `
+      ---------------------------------
+      Prueba de funciones con Async/Await
+      ---------------------------------\n`
       console.log("---------------------------------")
       console.log("Prueba de funciones con Async/Await ")
       console.log("---------------------------------")
-      pruebasPromesas()
-    }, 9000)
-  
+      pruebaAsync()
+    }, 15000)
+
 }
 
 pruebas()
