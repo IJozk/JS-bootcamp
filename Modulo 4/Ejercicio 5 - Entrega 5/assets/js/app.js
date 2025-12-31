c8de3322f129e3b354bb107bdd68dc9e8de5dfe1
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2025-12-31 01:39:05 -0300

    feat: termino ejerciocio 5 modulo 4

--- Modulo 4/Ejercicio 5 - Entrega 5/assets/js/app.js
+++ Modulo 4/Ejercicio 5 - Entrega 5/assets/js/app.js
@@ -24,7 +24,7 @@ cargar_xhr_btn.addEventListener("click", () =>{
   const xhr = new XMLHttpRequest();
 
   // Se define que tipo de solicitud enviaremos (GET), la url donde enviaremos la solicitud y si es async o no.
-  xhr.open('GET', url_users, true);
+  xhr.open('GET', url_birds, true);
 
   // Tipo de respuesta esperada
   // xhr.responseType = 'json';
@@ -34,16 +34,15 @@ cargar_xhr_btn.addEventListener("click", () =>{
     if (xhr.status === 200) {
       let respuesta = JSON.parse(xhr.response)
       console.log(respuesta); // Datos recibidos
+      agregarContenidoVista(respuesta)
     } else {
       console.error('Error del servidor:', xhr.status);
     }
   };
-
   // Se ejecuta cuando ocurre un error de conexion
   xhr.onerror = () => {
     console.error('Error de red, la petición no se envió');
   };
-
   xhr.send();
 })
 
@@ -78,65 +77,99 @@ cargar_fetch_btn.addEventListener("click", () => {
     .then(data => {
       // Mostrar la data obtenida
       div_resultado.innerHTML = ""
-
+      agregarContenidoVista(data)
       console.log(data);
-      data.forEach(bird => {
-        console.log(`Obteniendo info desde url : ${url_birds}/${bird.uid}`)
-        obtenerinfo(`${url_birds}/${bird.uid}`).then( info =>{
-          if (!info.didyouknow){
-            div_resultado.innerHTML +=
-            `<div class="card" style="width: 18rem;">
-              <img src="${bird.images.main}" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${bird.name.spanish}</h5>
-                <p class="card-text"> "Su nombre ya lo dice todo, que quieres saber?"</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>`
-            
-          }else {
-            div_resultado.innerHTML +=
-            `<div class="card" style="width: 18rem;">
-              <img src="${bird.images.main}" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">${bird.name.spanish}</h5>
-                <p class="card-text">${ info.didyouknow == "" ? "Su nombre ya lo dice todo, que quieres saber?" :  info.didyouknow}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>`
-          }
-          
-        
-        }
-        )  
-      });
     }).catch(
       console.error(error)
     )
 })
 
+const agregarContenidoVista = (arrayContenido) => {
+  arrayContenido.forEach(bird => {
+    console.log(`Obteniendo info desde url : ${url_birds}/${bird.uid}`)
+    obtenerinfo(`${url_birds}/${bird.uid}`).then( info =>{
+      if (!info.didyouknow){
+        div_resultado.innerHTML +=
+        `<div class="col-md-3">
+          <div class="card" style="width: 18rem;">
+            <img src="${bird.images.main}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${bird.name.spanish}</h5>
+              <p class="card-text"> "Su nombre ya lo dice todo, que quieres saber?"</p>
+            </div>
+          </div>
+        </div>`
+        
+      }else {
+        div_resultado.innerHTML +=
+        `<div class="col-sm-12 col-md-6 col-lg-4">
+          <div class="card" style="width: 18rem;">
+            <img src="${bird.images.main}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${bird.name.spanish}</h5>
+              <p class="card-text">${ info.didyouknow == "" ? "Su nombre ya lo dice todo, que quieres saber?" :  info.didyouknow}</p>
+              
+            </div>
+          </div>
+        </div>`
+      }
+    })  
+  });
+}
+
 async function obtenerinfo(url) {
   try {
     const response = await fetch(url);
-    
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
     const data = await response.json();
-    
     // Verifica que exista antes de usar
     if (data && data.didyouknow) {
       console.log(data.didyouknow);
     } else {
       console.warn("No hay datos de 'didyouknow' para este pájaro");
     }
-    
     return data;
-    
   } catch (error) {
     console.error(`Error al obtener info de ${url}:`, error.message);
     return null; // Devuelve null en lugar de romper
   }
 }
 
+const apiKey = 'eQKRlwatpLGfQLlBb3TRANAXo6vm22pe';
+const inputBusqueda = document.getElementById("input-busqueda");
+const btnBusqueda = document.getElementById("buscar-gif");
+const contImageResult = document.getElementById("gif");
+const url_giphy = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}`;
+
+
+btnBusqueda.addEventListener( "click" , () => {
+  const palabraClave = inputBusqueda.value
+  let url_busqueda = `${url_giphy}&q=${encodeURIComponent(palabraClave)}`
+  fetch(url_busqueda)
+  .then(response => {
+    return response.json()
+  })
+  .then(urls => {
+    if(urls.data.length > 0){
+      mostrarGif(urls.data[0].images.original.url, null)
+    }else{
+      console.log(urls.length)
+      mostrarGif(null, "No se encontro un gif")
+    }
+  })
+  .catch(error => console.error(error));
+})
+
+const mostrarGif = (urlimage, error) => {
+  if (urlimage){
+    contImageResult.innerHTML = `<div class="col" id="gif">
+            <img src="${urlimage}" alt="" srcset="">
+        </div>`
+  }else{
+    contImageResult.innerHTML = `<div class="col" id="gif">
+            <p>${error}</p>
+        </div>`
+  }
+}
