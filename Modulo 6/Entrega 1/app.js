cc96399e8a55e09e11f7e10e1ffc207d1c9f4111
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2026-02-06 04:30:23 -0300

    feat: Modulo 6 Entrega 2

--- Modulo 6/Entrega 1/app.js
+++ Modulo 6/Entrega 1/app.js
@@ -4,11 +4,11 @@ const http = require('http');
 
 const server = http.createServer((req, res) =>{
     if (req.url === '/'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
         res.end('<h1>Servidor Ejecutandose en el puerto 3030.</h1>')
     }
     else{
-        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
         res.end('<h1 style="color: red">Ruta no encontrada en el servidor.</h1>')
     }
 });
