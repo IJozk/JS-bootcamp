91baa4dba1fadebaa5068c9beccf18cd18a8852e
Jorge Riffo <107148859+IJozk@users.noreply.github.com>
2026-02-26 01:55:07 -0300

    feat: Entrega 1 Modulo 7
    Prueba conexión con DB y configuración con express, pg y dotenv.

---
+++ Modulo 7/Entrega 1/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const pool = require('../config/db'); // Importar la configuración de db.js
+const app = express();
+
+// Ruta para probar la conexión
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: 'Conexión exitosa', time: result.rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+});
